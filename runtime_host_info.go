80b92a62ef48e4d64035371168a63ef3afc91d6e
Ed Dao <ed@intellogic.com>
2024-01-21 14:56:25 -0800

    fix instance metadata

--- runtime_host_info.go
+++ runtime_host_info.go
@@ -14,9 +14,22 @@ type RutimeHostInfo struct {
 }
 
 func GetHostInstanceInfo() RutimeHostInfo {
-	resp, err := http.Get("http://metadata.google.internal/instance/id")
+	retval := RutimeHostInfo{
+		BuildId: os.Getenv("BUILD_ID"),
+		ImageId: os.Getenv("IMAGE_DIGEST"),
+	}
+
+	req, err := http.NewRequest("GET", "http://metadata.google.internal/instance/id", nil)
+	if err != nil {
+		log.Printf("GetHostInfo: Error metadata request: %s", err.Error())
+		return retval
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("GetHostInfo: Error metadata get: %s", err.Error())
+		return retval
 	}
 	defer resp.Body.Close()
 
@@ -24,10 +37,6 @@ func GetHostInstanceInfo() RutimeHostInfo {
 	if err != nil {
 		log.Printf("GetHostInfo: Error metadata read: %s", err.Error())
 	}
-
-	return RutimeHostInfo{
-		InstanceId: string(instanceID),
-		BuildId:    os.Getenv("BUILD_ID"),
-		ImageId:    os.Getenv("IMAGE_DIGEST"),
-	}
+	retval.InstanceId = string(instanceID)
+	return retval
 }
