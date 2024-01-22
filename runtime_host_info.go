65f20dbc02f892ee9fc9207354e04b1c122e65f8
Ed Dao <ed@intellogic.com>
2024-01-21 16:35:22 -0800

    simplify host instance

--- runtime_host_info.go
+++ runtime_host_info.go
@@ -1,42 +1,21 @@
 package sitepages
 
 import (
-	"io"
-	"log"
-	"net/http"
 	"os"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type RutimeHostInfo struct {
-	InstanceId string `bson:"instance_id"`
-	BuildId    string `bson:"build_id"`
-	ImageId    string `bson:"image_id"`
+	Id      primitive.ObjectID `bson:"_id"`
+	BuildId string             `bson:"build_id"`
+	ImageId string             `bson:"image_id"`
 }
 
-func GetHostInstanceInfo() RutimeHostInfo {
-	retval := RutimeHostInfo{
+func NewHostInstanceInfo() RutimeHostInfo {
+	return RutimeHostInfo{
+		Id:      primitive.NewObjectID(),
 		BuildId: os.Getenv("BUILD_ID"),
 		ImageId: os.Getenv("IMAGE_DIGEST"),
 	}
-
-	req, err := http.NewRequest("GET", "http://metadata.google.internal/instance/id", nil)
-	if err != nil {
-		log.Printf("GetHostInfo: Error metadata request: %s", err.Error())
-		return retval
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Printf("GetHostInfo: Error metadata get: %s", err.Error())
-		return retval
-	}
-	defer resp.Body.Close()
-
-	instanceID, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("GetHostInfo: Error metadata read: %s", err.Error())
-	}
-	retval.InstanceId = string(instanceID)
-	return retval
 }
