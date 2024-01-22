816ca14334c90971462a596172adadde9339504f
Ed Dao <ed@intellogic.com>
2024-01-22 09:25:10 -0800

    add start time

--- runtime_host_info.go
+++ runtime_host_info.go
@@ -2,20 +2,23 @@ package sitepages
 
 import (
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type RutimeHostInfo struct {
-	Id      primitive.ObjectID `bson:"_id"`
-	BuildId string             `bson:"build_id"`
-	ImageId string             `bson:"image_id"`
+	Id        primitive.ObjectID `bson:"_id"`
+	BuildId   string             `bson:"build_id"`
+	ImageId   string             `bson:"image_id"`
+	StartTime time.Time          `bson:"start_time"`
 }
 
 func NewHostInstanceInfo() RutimeHostInfo {
 	return RutimeHostInfo{
-		Id:      primitive.NewObjectID(),
-		BuildId: os.Getenv("BUILD_ID"),
-		ImageId: os.Getenv("IMAGE_DIGEST"),
+		Id:        primitive.NewObjectID(),
+		BuildId:   os.Getenv("BUILD_ID"),
+		ImageId:   os.Getenv("IMAGE_DIGEST"),
+		StartTime: time.Now(),
 	}
 }
