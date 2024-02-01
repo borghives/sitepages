9b3407372fe10af19e2be82cec2bc4733d32511d
Ed Dao <ed@intellogic.com>
2024-02-01 10:26:46 -0800

    add error helper

--- session_error.go
+++ session_error.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+var SESSION_ERROR_NO_SESSION = 1 << 1
+var SESSION_ERROR_DECODING_FAILED = 1 << 2
+var SESSION_ERROR_SESSION_EXPIRED = 1 << 3
+var SESSION_ERROR_IP_MISMATCH = 1 << 4
+var SESSION_ERROR_CLIENT_MISMATCH = 1 << 4
+
 type WebSessionError struct {
 	Message string
 	Code    int
@@ -15,10 +21,25 @@ func (e *WebSessionError) Error() string {
 	return fmt.Sprintf("Error 0x%X: %s", e.Code, e.Message)
 }
 
-var SESSION_ERROR_NO_SESSION = 1 << 1
-var SESSION_ERROR_DECODING_FAILED = 1 << 2
-var SESSION_ERROR_SESSION_EXPIRED = 1 << 3
-var SESSION_ERROR_IP_MISMATCH = 1 << 4
+func (e *WebSessionError) HasNoSession() bool {
+	return (e.Code & SESSION_ERROR_NO_SESSION) != 0
+}
+
+func (e *WebSessionError) DecodingFailed() bool {
+	return (e.Code & SESSION_ERROR_DECODING_FAILED) != 0
+}
+
+func (e *WebSessionError) SessionExpired() bool {
+	return (e.Code & SESSION_ERROR_SESSION_EXPIRED) != 0
+}
+
+func (e *WebSessionError) IpMismatch() bool {
+	return (e.Code & SESSION_ERROR_IP_MISMATCH) != 0
+}
+
+func (e *WebSessionError) ClientMismatch() bool {
+	return (e.Code & SESSION_ERROR_CLIENT_MISMATCH) != 0
+}
 
 // VerifySession verifies that the session is valid
 
@@ -52,6 +73,13 @@ func GetAndVerifySession(r *http.Request) (*WebSession, error) {
 	if realip != string(session.FromIp) {
 		sessionError.Message += "IP mismatch; "
 		sessionError.Code |= SESSION_ERROR_IP_MISMATCH
+	}
+
+	client := GetClientSignature(r)
+	clientHash := HashToIdHexString(client)
+	if clientHash != session.ClientHash {
+		sessionError.Message += "Client mismatch; "
+		sessionError.Code |= SESSION_ERROR_CLIENT_MISMATCH
 	}
 
 	if sessionError.Code == 0 {
