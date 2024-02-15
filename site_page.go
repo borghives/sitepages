c4aff6ab5d57c7eec5dfeae2b68425e2bfae0a60
Ed Dao <ed@intellogic.com>
2024-02-15 09:32:54 -0800

    captialize

--- site_page.go
+++ site_page.go
@@ -45,8 +45,8 @@ type Stanza struct {
 	Context         primitive.ObjectID `xml:"context,omitempty" json:"Context,omitempty" bson:"context,omitempty"`
 	BasePage        primitive.ObjectID `xml:"basepage" json:"BasePage" bson:"base_page"`
 	PreviousVersion primitive.ObjectID `xml:"previousversion" json:"PreviousVersion" bson:"previous_version"`
-	ChunkIndex      uint16             `xml:"chunkidx" json:"chunkIdx" bson:"-"`       //only for control and not for persist in state
-	ChunkOffset     uint16             `xml:"chunkoffset" json:"chunkoffset" bson:"-"` //only for control and not for persist in state
+	ChunkIndex      uint16             `xml:"chunkidx" json:"ChunkIdx" bson:"-"`       //only for control and not for persist in state
+	ChunkOffset     uint16             `xml:"chunkoffset" json:"ChunkOffset" bson:"-"` //only for control and not for persist in state
 }
 
 type Synapse struct {
