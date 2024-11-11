2da569b7dee29157fd8b59c623e0867134fdf50b
Ed Dao <ed@intellogic.com>
2024-11-10 19:15:26 -0800

    add template server mux

--- template_handler.go
+++ template_handler.go
@@ -11,6 +11,49 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type LinkAndIdPageMap map[string]map[string]*SitePage
+type LinkPageMap map[string]*SitePage
+
+type TemplateServerMux struct {
+	Mux       *http.ServeMux
+	templates map[string]*template.Template
+}
+
+func NewTemplateServerMux(frontPagesFolder string, templateComponentFolder string) *TemplateServerMux {
+	return &TemplateServerMux{
+		Mux:       http.NewServeMux(),
+		templates: LoadAllTemplatePages(frontPagesFolder, templateComponentFolder),
+	}
+}
+
+func (t *TemplateServerMux) Handle(pattern string, page string, requireAuth bool) {
+	template, exists := t.templates[page]
+	if !exists {
+		log.Fatal("Page Template doesn't exists", page)
+	}
+	t.Mux.Handle(pattern, TemplateHandler{template, requireAuth})
+}
+
+func (t *TemplateServerMux) HandlePageByLinkAndId(pattern string, page string, mapping LinkAndIdPageMap) {
+	template, exists := t.templates[page]
+	if !exists {
+		log.Fatal("Page Template doesn't exists", page)
+	}
+	t.Mux.Handle(pattern, PageByIdTemplateHandler{template, mapping})
+}
+
+func (t *TemplateServerMux) HandlePageByLink(pattern string, page string, mapping LinkPageMap) {
+	template, exists := t.templates[page]
+	if !exists {
+		log.Fatal("Page Template doesn't exists", page)
+	}
+	t.Mux.Handle(pattern, PageLinksTemplateHandler{template, mapping})
+}
+
+func (t *TemplateServerMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.Mux.ServeHTTP(w, r)
+}
+
 type TemplateHandler struct {
 	WebTemplates *template.Template
 	RequireAuth  bool
